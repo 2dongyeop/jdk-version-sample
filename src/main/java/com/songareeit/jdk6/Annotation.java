7526fddd8217ff5b277f737cefbc424588979375
leedongyeop <ldy_1204@naver.com>
2023-10-22 20:43:35 +0900

    feat : JDK 1.6 변화 예시 코드

---
+++ src/main/java/com/songareeit/jdk6/Annotation.java
@@ -0,0 +1,43 @@
+package com.songareeit.jdk6;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface MyAnnotation {
+
+    String value() default "Default Value";
+
+    int count() default 0;
+}
+
+public class Annotation {
+    @MyAnnotation(value = "Custom Value", count = 10)
+    public void myAnnotatedMethod() {
+        System.out.println("Annotation.myAnnotatedMethod");
+    }
+
+    public static void main(String[] args) throws NoSuchMethodException {
+
+        // Annotation (예제) 클래스 추출
+        Class<Annotation> clazz = Annotation.class;
+
+        // (애너테이션을 작성해놓은) 메서드 추출
+        Method method = clazz.getMethod("myAnnotatedMethod");
+
+        // 추출한 메서드로부터 애너테이션 추출
+        MyAnnotation annotation = method.getAnnotation(MyAnnotation.class);
+
+        // 애너테이션 내용 검증
+        if (annotation != null) {
+            String value = annotation.value();
+            int count = annotation.count();
+            System.out.println("Value: " + value);
+            System.out.println("Count: " + count);
+        }
+    }
+}
