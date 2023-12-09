b08206d19fc1b63b35d1570b266e978c32d780ff
leedongyeop <ldy_1204@naver.com>
2023-12-09 22:03:14 +0900

    feat : JDK 18 Features 예시 코드

---
+++ src/main/java/com/songareeit/jdk18/CodeSnippetsJavaDocExample.java
@@ -0,0 +1,11 @@
+package com.songareeit.jdk18;
+
+/**
+ * JDK 18에 추가된 Java Doc에서 코드 스니펫 작성 기능
+ * {@snippet :
+ * String example = "이런식으로 Java Doc에 예시 코드를 작성할 때 사용할 수 있어요";
+ * System.out.println(example);
+ *}
+ */
+public class CodeSnippetsJavaDocExample {
+}
